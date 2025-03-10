e28f48ece42ec8cc1ee24aeb3e4c88b8e190ade3
ssmithope <145027277+ssmithope@users.noreply.github.com>
2025-03-10 17:20:04 -0500

    initial commit

--- server.js
+++ server.js
@@ -42,3 +42,18 @@ const host = process.env.HOST
 app.listen(port, () => {
   console.log(`app listening on ${host}:${port}`)
 })
+
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+});
+
+
+const path = require('path');
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+
+const express = require('express');
+app.use('/public', express.static(path.join(__dirname, 'public')));
