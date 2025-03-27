981acfca0deee0bdf132ff246a14e4955d2b5c38
ssmithope <145027277+ssmithope@users.noreply.github.com>
2025-03-27 03:08:09 -0500

    Initial commit

--- server.js
+++ server.js
@@ -1,18 +1,24 @@
-const baseController = require("./controllers/baseController");
-
 const express = require("express");
 const path = require("path");
-const staticRoutes = require("./routes/static");
+
+// Import routes and controllers
+const baseController = require("./controllers/baseController");
 const inventoryRoutes = require("./routes/inventoryRoute");
+const staticRoutes = require("./routes/static");
 
 const app = express();
 
+// Set the view engine to ejs
 app.set("view engine", "ejs");
+
+// Set up static folder
 app.use(express.static(path.join(__dirname, "public")));
 
+// Routes
 app.use("/", staticRoutes);
 app.use("/inventory", inventoryRoutes);
 
+// Error handling middleware
 app.use((err, req, res, next) => {
     console.error(`Error at "${req.originalUrl}": ${err.message}`);
     res.status(err.status || 500).render("errors/error", {
@@ -21,6 +27,7 @@ app.use((err, req, res, next) => {
     });
 });
 
+// Start the server
 const PORT = 3000;
 app.listen(PORT, () => console.log(`Server is running on http://localhost:${PORT}`));
 
