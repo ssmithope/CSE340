551e6c197ddb283d860f9cfcae31ff751184ec21
ssmithope <145027277+ssmithope@users.noreply.github.com>
2025-03-29 09:37:06 -0500

    Initial commit

--- controllers/baseController.js
+++ controllers/baseController.js
@@ -7,22 +7,35 @@ baseController.buildHome = (req, res) => {
 };
 
 baseController.buildCustom = (req, res) => {
-  res.render("custom", { title: "Custom" });
+  res.render("custom", { 
+    title: "Custom", 
+    image: "/images/upgrades/flame-decals.jpg", 
+    details: "Personalize your vehicle with flame decals for a bold, standout look." 
+  });
 };
 
 baseController.buildSedan = (req, res) => {
-  res.render("sedan", { title: "Sedan" });
+  res.render("sedan", { 
+    title: "Sedan", 
+    image: "/images/vehicles/sedan.jpg", 
+    details: "Stylish, comfortable, and perfect for city driving." 
+  });
 };
 
 baseController.buildSUV = (req, res) => {
-  res.render("suv", { title: "SUV" });
+  res.render("suv", { 
+    title: "SUV", 
+    image: "/images/vehicles/suv.jpg", 
+    details: "Spacious and powerful, ready for your next adventure." 
+  });
 };
 
 baseController.buildTruck = (req, res) => {
-  res.render("truck", { title: "Truck" });
+  res.render("truck", { 
+    title: "Truck", 
+    image: "/images/vehicles/truck.jpg", 
+    details: "Built tough to handle any job, big or small." 
+  });
 };
-
-module.exports = baseController;
-
 
 module.exports = baseController;
