874845ee77090765993af9008f139e7c0fef2820
ssmithope <145027277+ssmithope@users.noreply.github.com>
2025-03-29 09:17:35 -0500

    Initial commit

--- server.js
+++ server.js
@@ -5,24 +5,23 @@ const helmet = require("helmet");
 const morgan = require("morgan");
 const expressLayouts = require("express-ejs-layouts");
 const baseController = require("./controllers/baseController");
-const errorController = require("./controllers/errorController");
 
 const app = express();
 
 /* *******************************************
  * Middleware Setup
  * ****************************************** */
-app.use(helmet());
-app.use(morgan("dev"));
-app.use(express.static(path.join(__dirname, "public")));
+app.use(helmet()); // Secure HTTP headers
+app.use(morgan("dev")); // Logging
+app.use(express.static(path.join(__dirname, "public"))); // Serve static files
 
 /* *******************************************
  * View Engine and Templates
  * ****************************************** */
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
-app.use(expressLayouts);
-app.set("layout", "./layouts/layout");
+app.set("views", path.join(__dirname, "views")); // Set views directory
+app.set("view engine", "ejs"); // EJS as the template engine
+app.use(expressLayouts); // Enable layouts
+app.set("layout", "./layouts/layout"); // Default layout file
 
 /* *******************************************
  * Routes
@@ -36,18 +35,26 @@ app.get("/truck", baseController.buildTruck);
 /* *******************************************
  * Error Handling Middleware
  * ****************************************** */
+// Handle 404 errors
 app.use((req, res) => {
-  res.status(404).render("errors/errors", { title: "404 Error", message: "Page not found" });
+  res.status(404).render("errors/errors", { 
+    title: "404 Error", 
+    message: "Page not found" 
+  });
 });
 
+// Handle other errors
 app.use((err, req, res, next) => {
   console.error(`Error: ${err.message}`);
   res.status(err.status || 500).render("errors/errors", {
     title: `${err.status || 500} Error`,
-    message: err.message,
+    message: err.message || "Internal Server Error",
   });
 });
 
+/* *******************************************
+ * Start Server
+ * ****************************************** */
 const PORT = process.env.PORT || 3000;
 const HOST = process.env.HOST || "localhost";
 
@@ -55,9 +62,8 @@ app.listen(PORT, () => {
   console.log(`Server running at http://${HOST}:${PORT}`);
 });
 
-
+// shutdown
 process.on("SIGINT", () => {
   console.log("Shutting down server...");
   process.exit(0);
 });
-
