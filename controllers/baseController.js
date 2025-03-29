85766296d1eff6c92411e36d7ff207c2fa3f83ff
ssmithope <145027277+ssmithope@users.noreply.github.com>
2025-03-29 12:21:03 -0500

    Initial commit

--- controllers/baseController.js
+++ controllers/baseController.js
@@ -2,19 +2,27 @@ const { getNav } = require("../utilities/navigation");
 
 const baseController = {};
 
+// Reusable Upgrades Array
+const upgrades = [
+  { name: "Flame Decals", image: "/images/upgrades/flame-decals.jpg" },
+  { name: "Bumper Stickers", image: "/images/upgrades/bumper-stickers.jpg" },
+  { name: "Hub Caps", image: "/images/upgrades/hub-caps.jpg" },
+];
+
 // Build Home Page
 baseController.buildHome = async (req, res, next) => {
   try {
     const nav = await getNav();
     res.render("index", { title: "Home", nav });
   } catch (error) {
-    next(error); // Pass error to middleware
+    next(error);
   }
 };
 
 // Build Custom Page
 baseController.buildCustom = async (req, res, next) => {
   try {
+    const nav = await getNav();
     const vehicle = {
       make: "Custom",
       model: "Flame Decals Edition",
@@ -25,23 +33,16 @@ baseController.buildCustom = async (req, res, next) => {
       inv_description: "Personalize your ride with bold flame decals.",
     };
 
-    const upgrades = [
-      { name: "Flame Decals", image: "/images/upgrades/flame-decals.jpg" },
-      { name: "Bumper Stickers", image: "/images/upgrades/bumper-stickers.jpg" },
-      { name: "Hub Caps", image: "/images/upgrades/hub-caps.jpg" },
-    ];
-
-    res.render("custom", { title: `${vehicle.make} ${vehicle.model}`, vehicle, upgrades });
+    res.render("custom", { title: `${vehicle.make} - ${vehicle.model}`, vehicle, upgrades, nav });
   } catch (error) {
     next(error);
   }
 };
 
-// Repeat similar structures for Sedan, SUV, and Truck
-
 // Build Sedan Page
 baseController.buildSedan = async (req, res, next) => {
   try {
+    const nav = await getNav();
     const vehicle = {
       make: "Sedan",
       model: "Flame Decals Edition",
@@ -49,25 +50,19 @@ baseController.buildSedan = async (req, res, next) => {
       inv_price: 25000,
       inv_miles: 10000,
       inv_image: "/images/vehicles/sedan.jpg",
-      inv_description: "Personalize your ride with bold flame decals.",
+      inv_description: "A stylish sedan enhanced with bold flame decals.",
     };
 
-    const upgrades = [
-      { name: "Flame Decals", image: "/images/upgrades/flame-decals.jpg" },
-      { name: "Bumper Stickers", image: "/images/upgrades/bumper-stickers.jpg" },
-      { name: "Hub Caps", image: "/images/upgrades/hub-caps.jpg" },
-    ];
-
-    res.render("sedan", { title: `${vehicle.make} ${vehicle.model}`, vehicle, upgrades });
+    res.render("sedan", { title: `${vehicle.make} - ${vehicle.model}`, vehicle, upgrades, nav });
   } catch (error) {
     next(error);
   }
 };
 
-// Repeat similar structures for Sedan, SUV, and Truck
 // Build SUV Page
 baseController.buildSUV = async (req, res, next) => {
   try {
+    const nav = await getNav();
     const vehicle = {
       make: "SUV",
       model: "Flame Decals Edition",
@@ -75,26 +70,19 @@ baseController.buildSUV = async (req, res, next) => {
       inv_price: 25000,
       inv_miles: 10000,
       inv_image: "/images/vehicles/suv.jpg",
-      inv_description: "Personalize your ride with bold flame decals.",
+      inv_description: "A powerful SUV with bold flame decals.",
     };
 
-    const upgrades = [
-      { name: "Flame Decals", image: "/images/upgrades/flame-decals.jpg" },
-      { name: "Bumper Stickers", image: "/images/upgrades/bumper-stickers.jpg" },
-      { name: "Hub Caps", image: "/images/upgrades/hub-caps.jpg" },
-    ];
-
-    res.render("suv", { title: `${vehicle.make} ${vehicle.model}`, vehicle, upgrades });
+    res.render("suv", { title: `${vehicle.make} - ${vehicle.model}`, vehicle, upgrades, nav });
   } catch (error) {
     next(error);
   }
 };
 
-// Repeat similar structures for Sedan, SUV, and Truck
-
 // Build Truck Page
 baseController.buildTruck = async (req, res, next) => {
   try {
+    const nav = await getNav();
     const vehicle = {
       make: "Truck",
       model: "Flame Decals Edition",
@@ -102,21 +90,13 @@ baseController.buildTruck = async (req, res, next) => {
       inv_price: 25000,
       inv_miles: 10000,
       inv_image: "/images/vehicles/truck.jpg",
-      inv_description: "Personalize your ride with bold flame decals.",
+      inv_description: "This rugged truck is equipped with bold flame decals.",
     };
 
-    const upgrades = [
-      { name: "Flame Decals", image: "/images/upgrades/flame-decals.jpg" },
-      { name: "Bumper Stickers", image: "/images/upgrades/bumper-stickers.jpg" },
-      { name: "Hub Caps", image: "/images/upgrades/hub-caps.jpg" },
-    ];
-
-    res.render("truck", { title: `${vehicle.make} ${vehicle.model}`, vehicle, upgrades });
+    res.render("truck", { title: `${vehicle.make} - ${vehicle.model}`, vehicle, upgrades, nav });
   } catch (error) {
     next(error);
   }
 };
-
-// Repeat similar structures for Sedan, SUV, and Truck
 
 module.exports = baseController;
