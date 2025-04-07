9b6473c78d74e7b9142567e74fd9990d3f70270f
ssmithope <145027277+ssmithope@users.noreply.github.com>
2025-04-06 19:13:25 -0500

    Initial commit

--- database/index.js
+++ database/index.js
@@ -1,11 +1,8 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    user: 'your_database_user',
-    host: 'localhost',
-    database: 'your_database_name',
-    password: 'your_database_password',
-    port: 5432, // Default port for PostgreSQL
+    connectionString: process.env.DATABASE_URL, // Use the database URL from .env
+    ssl: { rejectUnauthorized: false }, // Enable SSL for secure connections on Render
 });
 
 module.exports = pool;
