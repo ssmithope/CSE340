bc4f83b6efa20cd2f3492405b70183ac2c4641c3
ssmithope <145027277+ssmithope@users.noreply.github.com>
2025-04-06 20:51:16 -0500

    Initial commit

--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -1,11 +1,32 @@
-const express = require("express");
-const router = express.Router();
-const invController = require("../controllers/invController");
+// Needed Resources 
+const express = require("express")
+const router = new express.Router() 
+const invController = require("../controllers/invController")
+const errorController = require('../controllers/errorController')
+utilities = require('../utilities')
 
-// Route for classification views (e.g., sedan, SUV, etc.)
-router.get("/classification/:classification", invController.classificationView);
 
-// Route for vehicle detail views
-router.get("/:id", invController.getVehicleDetail);
+
+
+// Route to build inventory by classification view
+router.get("/type/:classificationId", invController.buildByClassificationId);
+
+// Route to show vehicle details by inv_id
+router.get("/detail/:invId", invController.showVehicleDetail);
+
+router.get("/", invController.buildManagementView);
+
+router.get('/add-classification', invController.showAddClassification);
+router.post('/add-classification', invController.addClassification);
+
+router.get(
+    "/getInventory/:classification_id",
+    utilities.handleErrors(invController.getInventoryJSON)
+  );
+  
+
+router.get('/add-inventory', invController.addInventory);
+router.post('/add-inventory', invController.addInventory); 
+router.get('/trigger-error', errorController.throwError);
 
 module.exports = router;
